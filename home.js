252a6405100b4ddcd2a3756e49fc48ea49b01c4c
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-02-20 17:26:28 +0100

    Configure webpack.config.js

---
+++ home.js
@@ -0,0 +1,18 @@
+function home() {
+    const heroTitle = document.createElement('h1');
+    const contentInfo = document.createElement('p');
+    const timeOpenList = document.createElement('ul');
+    const locationInfo = document.createElement('p');
+
+
+
+
+    heroTitle.innerText = 'Welcome to my new restaurant!'
+    heroTitle.classList.add('heroTitle');
+    contentInfo.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint obcaecati cum porro totam enim earum culpa, voluptatibus, voluptas non id doloribus doloremque? Aut laudantium voluptate perferendis, eveniet autem laboriosam. Id.';
+
+
+
+
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js')
+    },
     output: {
-        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        //when we run npm run dev it will open the browser automatically
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
     },
     module: {
         rules: [{
@@ -12,4 +31,12 @@ module.exports = {
             use: ['style-loader', 'css-loader'],
         }, ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html',
+        })
+    ]
+
 };
