9e6fa488102870b42701ab252397f48a8a919669
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-02-20 17:39:48 +0100

    Add assets,styles directories

---
+++ dist/bundlee080491db0855dfe1933.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('test')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlZTA4MDQ5MWRiMDg1NWRmZTE5MzMuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxtQiIsInNvdXJjZXMiOlsid2VicGFjazovL3Byb2plY3QtcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCd0ZXN0JykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
