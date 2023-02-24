7186e2a09c612ae923c8bbbb38d2006deedb5430
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-02-24 12:24:13 +0100

    Create phoneContainer and addressContainer in contactpage.js

--- src/components/contactpage.js
+++ src/components/contactpage.js
@@ -1,6 +1,42 @@
+function createPhoneContainer() {
+    const phoneInfoContainer = document.createElement('div');
+    phoneInfoContainer.classList.add('phone-container');
+    const phoneTitle = document.createElement('h2');
+    phoneTitle.innerHTML = 'PHONE NUMBER:';
+    const phoneNumber = document.createElement('p');
+    phoneNumber.innerHTML = '305-799-0658';
+    phoneInfoContainer.append(phoneTitle, phoneNumber);
+}
+
+function createAddressContainer() {
+    const addressInfoContainer = document.createElement('div');
+    addressInfoContainer.classList.add('address-container');
+    const addressTitle = document.createElement('h2');
+    addressTitle.innerHTML = 'ADDRESS:';
+    const addressInfo = document.createElement('p');
+    addressInfo.innerHTML = '7294 Bayberry Avenue Jacksonville, FL 32205';
+    addressInfoContainer.append(addressTitle, addressInfo);
+
+}
+
+
+function createContactPage() {
+    const content = document.querySelector('content');
+    const contentContainer = document.createElement('div');
+    contentContainer.classList.add('content-container');
+
+
+
+}
+
+
 function contactPage() {
+    // const content = document.getElementById('content');
     const pageTitle = document.querySelector('.hero-title');
     pageTitle.innerHTML = 'CONTACT'
+
+
+
 }
 
 export default contactPage;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles/style.css';
 import starterPage from './components/starterpage';
 import menuPage from './components/menupage';
 import homePage from './components/homepage';
-
+import contactPage from './components/contactpage';
 const homeBtn = document.querySelector('.home-button');
 const menuBtn = document.querySelector('.menu-button');
 const contactBtn = document.querySelector('.contact-button');
@@ -25,6 +25,20 @@ homeBtn.addEventListener('click', () => {
         content.removeChild(menuDrinksContainer);
     }
     homePage();
+})
+
+contactBtn.addEventListener('click', () => {
+    const menuDrinksContainer = document.querySelector('.menu-drinks-container');
+    const aboutContainer = document.querySelector('.about-container');
+    const hoursContainer = document.querySelector('.hours-container')
+    aboutContainer.classList.add('hidden')
+    hoursContainer.classList.add('hidden')
+    if (menuDrinksContainer) {
+        menuDrinksContainer.classList.add('hidden');
+        content.removeChild(menuDrinksContainer);
+    }
+    contactPage();
+
 })
 
 
